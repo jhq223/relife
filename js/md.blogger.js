185f063aa52e1fd4f337e80034be90c767cd4e3d
今何求 <62083936+jhq223@users.noreply.github.com>
2021-09-08 20:21:37 +0800

    Update md.blogger.js

--- js/md.blogger.js
+++ js/md.blogger.js
@@ -1,46 +1,24 @@
-//
-// markdown-highlight-in-blogger.js -- javascript for using Markdown in Blogger
-// Based on Francis Tang's http://blog.chukhang.com/2011/09/markdown-in-blogger.html
-// Copyright (c) 2017 Divya van Mahajan
-//
-// Redistributable under a BSD-style open source license.
-// Documentation: https://github.com/cs905s/md-in-blogger
-//
+<script>
+var showdownOption = {
+  'simplifiedAutoLink': true,
+  'strikethrough': true,
+  'tables': true,
+  'tasklists': true
+}
 
-// namespace
-var MarkdownHighlightInBlogger = {};
-// From http://erlend.oftedal.no/blog/?blogid=14
-MarkdownHighlightInBlogger.unescapeHTML = function (html) {
-  var htmlNode = document.createElement("DIV");
-  htmlNode.innerHTML = html;
-  if(htmlNode.innerText !== undefined)
-    return htmlNode.innerText; // IE
-  return htmlNode.textContent; // FF
-};
+var converter = new showdown.Converter(showdownOption);
 
-MarkdownHighlightInBlogger.convertMD = function () {
-  try {
+var posts = document.querySelectorAll(".post-body");
 
-    console.info('Converting markdown using jQuery');
+Array.prototype.forEach.call(posts, function (el, i) {
+  var idx = el.innerHTML.indexOf("markdown");
+  if (idx != -1 && idx <= 1) {
+    let md = el.innerHTML
+      .replace(/\n&amp;gt;/g, '\n>')  // fix quotes
+      .replace("markdown", "");   // remove flag
 
-    // showdown renderer
-    var converter = new showdown.Converter({});
-    converter.setFlavor('github');
-    $('pre.markdown').each(function (i, block) {
-      //var rawtext = MarkdownHighlightInBlogger.unescapeHTML(block.innerText);
-      var rawtext = block.innerText;
-      var md_html = converter.makeHtml(rawtext);
-      var md = $(md_html); //.css('border','3px solid blue');
-      md.insertBefore(block);
-      block.hidden = true;
-    });
-    $('pre code').each(function (i, block) {
-      hljs.highlightBlock(block);
-    });
-  } catch (exc) {
-    console.error(exc);
+    el.innerHTML = converter.makeHtml(md)
+      .replace(/&amp;amp;/g, "&amp;"); // remove redundant escape
   }
-};
-
-$(document).ready(MarkdownHighlightInBlogger.convertMD);
-
+});
+</script>
